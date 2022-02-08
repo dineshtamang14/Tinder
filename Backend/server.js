5f2547ab0e63ba326e0e1a8480b6225c90c67eff
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-02-08 11:31:44 +0530

    create post and get routes in backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,45 @@
 import express from "express"
 import mongoose from "mongoose"
+import Cards from "./dbCards.js";
 
 const app = express();
 const port = process.env.PORT || 8000
+const connection_url = "mongodb+srv://dinesh:dinesh1997@cluster0.cuuqa.mongodb.net/Tinder-DB?retryWrites=true&w=majority"
 
 //  Middlewares
+app.use(express.json());
 
 // DB config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 // API Endpoints
 app.get("/", (req, res) =>res.status(200).send("Hollo, world!"))    
     
+app.post("/tinder/card", (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if(err){
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    })
+});
+
+app.get("/tinder/cars", (req, res) => {
+    Cards.find((err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+})
+
 app.listen(port, () => {
     console.log(`server is running on ${port}`);
 });
